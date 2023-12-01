a105d8f2b9a0e6b06748b12c43d22ba36d350fda
StripedMonkey <monkeyinastripedshirt@gmail.com>
2023-12-01 03:31:27 -0500

    Templates

---
+++ daily-template/src/bin/part1.rs
@@ -0,0 +1,13 @@
+fn main() {
+    let file = include_str!("../../input1.txt");
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn first_test() {
+    }
+}
---
+++ daily-template/src/bin/part2.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let file = include_str!("../../input2.txt");
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn first_test() {
+    }
+}
