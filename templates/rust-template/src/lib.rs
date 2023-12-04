cc94a0562db558418dd01ea74bed7d21d675b36e
StripedMonkey <monkeyinastripedshirt@gmail.com>
2023-12-04 02:53:46 -0500

    make static_read pub

--- templates/rust-template/src/lib.rs
+++ templates/rust-template/src/lib.rs
@@ -13,7 +13,7 @@ lazy_static!{
 // Read a file path relative to the parent of the current day's project directory.
 // Each day contains the inputs and examples for that day in the the parent, so I can 
 // (in theory/later) share the inputs between multiple languages. Pretend like I'll actually do that.
-fn static_read(file_path: &str) -> &'static str {
+pub fn static_read(file_path: &str) -> &'static str {
     let mut cwd = PathBuf::from_str(env!("CARGO_MANIFEST_DIR")).expect("Infallible action failed!");
     cwd.pop();
     let file_path = cwd.join(file_path);
