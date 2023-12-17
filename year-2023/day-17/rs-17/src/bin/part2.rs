9e278d98cc3c0519b36fe647a4318a8cc3a10e4b
StripedMonkey <monkeyinastripedshirt@gmail.com>
2023-12-17 11:49:29 -0500

    Day 17 - Part Two Pathfinding and Cleanup

---
+++ year-2023/day-17/rs-17/src/bin/part2.rs
@@ -0,0 +1,29 @@
+use rs_17::*;
+
+fn main() {
+    let file = *INPUT_1;
+    let map = parse_file(&file);
+    let result = map.puzzle::<4, 10>();
+    println!("The answer is {}", result); // 1283
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn first_test() {
+        let file = rs_17::static_read("example1.txt");
+        let map = parse_file(&file);
+        let result = map.puzzle::<4, 10>();
+        assert_eq!(result, 94);
+    }
+
+    #[test]
+    fn second_test() {
+        let file = rs_17::static_read("example2.txt");
+        let map = parse_file(&file);
+        let result = map.puzzle::<4, 9>();
+        assert_eq!(result, 71);
+    }
+}
--- year-2023/day-17/rs-17/src/lib.rs
+++ year-2023/day-17/rs-17/src/lib.rs
@@ -18,3 +18,180 @@ pub fn static_read(file_path: &str) -> &'static str {
     let file = std::fs::read_to_string(file_path).expect("Failed to open file!");
     Box::leak(file.into_boxed_str())
 }
+
+use pathfinding::directed::dijkstra::dijkstra;
+
+pub struct HeatMap {
+    map: Vec<Vec<usize>>,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+#[derive(Debug, Clone, Hash, PartialEq, Eq)]
+struct CruciblePath<const MIN_TURN: usize, const MAX_TURN: usize> {
+    position: (usize, usize),
+    direction: Direction,
+    previous: usize,
+}
+
+impl<const MIN_TURN: usize, const MAX_TURN: usize> CruciblePath<MIN_TURN, MAX_TURN> {
+    fn new(direction: Direction, x: usize, y: usize) -> CruciblePath<MIN_TURN, MAX_TURN> {
+        CruciblePath {
+            position: (x, y),
+            direction,
+            previous: 0,
+        }
+    }
+
+    fn cost(&self, map: &HeatMap) -> usize {
+        map.get(self.position.0, self.position.1)
+    }
+
+    fn successors<'a>(
+        &self,
+        map: &'a HeatMap,
+    ) -> impl Iterator<Item = (CruciblePath<MIN_TURN, MAX_TURN>, usize)> + 'a {
+        let direction = self.direction;
+        let (current_x, current_y) = self.position;
+        let mut successors = Vec::new();
+
+        if self.previous >= MIN_TURN {
+            match direction {
+                Direction::Up | Direction::Down => {
+                    if current_x > 0 {
+                        successors.push(CruciblePath {
+                            position: (current_x - 1, current_y),
+                            direction: Direction::Left,
+                            previous: 1,
+                        });
+                    }
+                    if current_x < map.width() - 1 {
+                        successors.push(CruciblePath {
+                            position: (current_x + 1, current_y),
+                            direction: Direction::Right,
+                            previous: 1,
+                        });
+                    }
+                }
+                Direction::Left | Direction::Right => {
+                    if current_y > 0 {
+                        successors.push(CruciblePath {
+                            position: (current_x, current_y - 1),
+                            direction: Direction::Up,
+                            previous: 1,
+                        });
+                    }
+                    if current_y < map.height() - 1 {
+                        successors.push(CruciblePath {
+                            position: (current_x, current_y + 1),
+                            direction: Direction::Down,
+                            previous: 1,
+                        });
+                    }
+                }
+            }
+        }
+
+        'forward: { // for the break 'forward
+            if self.previous >= MAX_TURN {
+                break 'forward;
+            }
+            successors.push(CruciblePath {
+                position: {
+                    let (x, y) = self.position;
+                    match direction {
+                        Direction::Up if y > 0 => (x, y - 1),
+                        Direction::Down if y < map.height() - 1 => (x, y + 1),
+                        Direction::Left if x > 0 => (x - 1, y),
+                        Direction::Right if x < map.width() - 1 => (x + 1, y),
+                        _ => break 'forward,
+                    }
+                },
+                direction,
+                previous: self.previous + 1,
+            })
+        }
+        successors.into_iter().map(move |p| {
+            let cost = p.cost(&map);
+            (p, cost)
+        })
+    }
+}
+
+impl HeatMap {
+    fn width(&self) -> usize {
+        self.map[0].len()
+    }
+
+    fn height(&self) -> usize {
+        self.map.len()
+    }
+
+    pub fn puzzle<const MIN: usize, const MAX: usize>(&self) -> usize {
+        let right_start = CruciblePath::<MIN, MAX>::new(Direction::Right, 0, 0);
+        let right_path = dijkstra(
+            &right_start,
+            |p| p.successors(&self),
+            |p| p.position == (self.width() - 1, self.height() - 1) && p.previous >= MIN,
+        );
+        let down_start = CruciblePath::<MIN, MAX>::new(Direction::Down, 0, 0);
+        let down_path = dijkstra(
+            &down_start,
+            |p| p.successors(&self),
+            |p| p.position == (self.width() - 1, self.height() - 1) && p.previous >= MIN,
+        );
+        let path = match (right_path, down_path) {
+            (None, None) => None,
+            (Some(right), None) => Some(right),
+            (None, Some(down)) => Some(down),
+            (Some(right), Some(down)) => {
+                if right.1 < down.1 {
+                    Some(right)
+                } else {
+                    Some(down)
+                }
+            }
+        };
+
+        let Some(path) = path else {
+            panic!("No Path");
+        };
+        // self.print_map_highlight(path.0.iter().map(|p| p.position).collect_vec());
+        path.1
+    }
+
+    fn get(&self, x: usize, y: usize) -> usize {
+        self.map[y][x]
+    }
+
+    fn print_map_highlight(&self, highlights: Vec<(usize, usize)>) {
+        for (y, row) in self.map.iter().enumerate() {
+            for (x, cell) in row.iter().enumerate() {
+                if highlights.contains(&(x, y)) {
+                    print!("\x1b[31m{}\x1b[0m", cell);
+                } else {
+                    print!("{}", cell);
+                }
+            }
+            println!();
+        }
+    }
+}
+
+pub fn parse_file(file: &str) -> HeatMap {
+    let map = file
+        .lines()
+        .map(|line| {
+            line.chars()
+                .map(|c| c.to_digit(10).unwrap() as usize)
+                .collect()
+        })
+        .collect();
+    HeatMap { map }
+}
